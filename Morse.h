f237de1e6ec636aa71374f2ba87bd89d907d44c4
NikolaiPerepelitza <nikolaip17@riverdale.k12.or.us>
2015-04-01 10:19:46 -0700

    Update Morse.h

--- Morse.h
+++ Morse.h
@@ -1,11 +1,6 @@
 
 int pin = 13
 
-/*
-  Morse.h - Library for flashing Morse code.
-  Created by David A. Mellis, November 2, 2007.
-  Released into the public domain.
-*/
 #ifndef Morse_h
 #define Morse_h
 
